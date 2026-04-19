deae6c64f10909b07e56a96611b78cecda90374c
Daniela Arcanjo <51161118+0028dani@users.noreply.github.com>
2026-04-19 11:21:25 -0300

    Adicionando o script de testes de transferencias

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -7,15 +7,15 @@ describe('Login', () => {
   it('Login com dados validos deve permitir entrada no sistema', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('123456')
-    cy.contains('button' , 'Entrar').click()
+    cy.contains('button', 'Entrar').click()
 
-    cy.contains('h4' , 'Realizar Transferência').should('be.visible')
+    cy.contains('h4', 'Realizar Transferência').should('be.visible')
   })
 
   it('Login com dados invalidos deve apresentar mensagem de erro', () => {
     cy.get('#username').click().type('julio.lima')
     cy.get('#senha').click().type('654321')
-    cy.contains('button' , 'Entrar').click()
+    cy.contains('button', 'Entrar').click()
 
     cy.get('.toast').should('have.text', 'Erro no login. Tente novamente.')
   })
